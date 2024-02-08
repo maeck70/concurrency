59a7561edd883e18871c580c4f82934598a939e3
Marcel van Eck <vaneck.marcel@gmail.com>
2024-02-07 18:02:06 -0800

    initial commit

---
+++ data.go
@@ -0,0 +1,36 @@
+package main
+
+type Data struct {
+	Name   string
+	Age    int32
+	Result int32
+}
+
+var data = []Data{
+	{"James", 32, 0},
+	{"Moneypenny", 27, 0},
+	{"Quilincy", 64, 0},
+	{"Marcel", 54, 0},
+	{"M", 56, 0},
+	{"Q", 43, 0},
+	{"Bond", 7, 0},
+	{"Prince", 27, 0},
+	{"King", 64, 0},
+	{"Queen", 54, 0},
+	{"Princess", 56, 0},
+	{"Duke", 43, 0},
+	{"Duchess", 7, 0},
+	{"Earl", 27, 0},
+	{"Countess", 64, 0},
+	{"Count", 54, 0},
+	{"Baker", 56, 0},
+	{"Butcher", 43, 0},
+	{"Candlestickmaker", 7, 0},
+	{"Carpenter", 27, 0},
+	{"Clerk", 64, 0},
+	{"Cook", 54, 0},
+	{"Doctor", 56, 0},
+	{"Dyer", 43, 0},
+	{"Farmer", 7, 0},
+	{"Fisherman", 27, 0},
+}
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type chanData struct {
+	index int
+	value Data
+}
+
+func worker(ch <-chan chanData, wg *sync.WaitGroup) {
+	for task := range ch {
+		delay := time.Duration(rand.Float32()*2000) * time.Millisecond
+		data[task.index].Calculate(2.5, delay)
+		atomic.StoreInt32(&data[task.index].Result, data[task.index].Result)
+	}
+	wg.Done()
+}
+
+func main() {
+	ch := make(chan chanData)
+	var wg sync.WaitGroup
+
+	// Launch workers
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go worker(ch, &wg)
+	}
+
+	// Send tasks to workers
+	for i, value := range data {
+		ch <- chanData{i, value}
+	}
+
+	close(ch)
+	wg.Wait()
+
+	// Print results
+	for _, d := range data {
+		d.Print()
+	}
+}
+
+func (d Data) Print() {
+	fmt.Println(d.Name, d.Age, d.Result)
+}
+
+func (d *Data) Calculate(factor float32, delay time.Duration) {
+	d.Wait(delay)
+	d.Result = int32(float32(d.Age) * factor)
+	fmt.Printf("Calculated for %s: %d\n", d.Name, d.Result)
+}
+
+func (d Data) Wait(delay time.Duration) {
+	fmt.Printf("...Waiting %s for %v\n", d.Name, delay)
+	time.Sleep(delay)
+}
